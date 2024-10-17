b16c5e54ab70c2135f109de254468b8d732a4ee8
Xaanaii <faisalhafiz07@gmail.com>
2024-10-17 15:11:49 +0800

    edited page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 export default function About(){
     return (
         <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center text-white bg-black bg-opacity-40">
-            <h1>About us</h1>
+            <h1>About us faishal edit</h1>
         </div>
     );
 }
